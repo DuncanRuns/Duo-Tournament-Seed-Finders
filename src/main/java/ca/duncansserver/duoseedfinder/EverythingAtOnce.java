bb78bd3ef2f58b63fa0ec11681a374d58a7ca109
DuncanRL <59705125+DuncanRL@users.noreply.github.com>
2020-12-06 05:20:59 -0400

    Now in practice seed finder state

--- src/main/java/ca/duncansserver/duoseedfinder/EverythingAtOnce.java
+++ src/main/java/ca/duncansserver/duoseedfinder/EverythingAtOnce.java
@@ -4,10 +4,14 @@ public class EverythingAtOnce {
 
     public static void main(String[] args) {
 
+        System.out.println("Starting Darcoke Finder...");
         DarkForestFinder.main(args);
+        System.out.println("Darcoke Done, Starting Igloo Finder...");
         IglooFinder.main(args);
+        System.out.println("Igloo Done, Starting Temple/Savillage Finder...");
         SavannaVillageTempleFinder.main(args);
-        
+        System.out.println("Everything Done! :D");
+
     }
 
 }
