f8ca49ff6b09d653e9521680c11aefd6de2fe267
DuncanRL <59705125+DuncanRL@users.noreply.github.com>
2021-05-14 15:16:26 -0300

    stuff

--- src/main/java/ca/duncansserver/duoseedfinder/IglooFinder.java
+++ src/main/java/ca/duncansserver/duoseedfinder/IglooFinder.java
@@ -1,12 +1,5 @@
 package ca.duncansserver.duoseedfinder;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.time.Clock;
-
 import kaptainwutax.biomeutils.Biome;
 import kaptainwutax.biomeutils.source.BiomeSource;
 import kaptainwutax.biomeutils.source.OverworldBiomeSource;
@@ -17,115 +10,121 @@ import kaptainwutax.featureutils.structure.Village;
 import kaptainwutax.seedutils.mc.ChunkRand;
 import kaptainwutax.seedutils.mc.MCVersion;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Clock;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
 public class IglooFinder {
 
     public static final Village village = new Village(MCVersion.v1_14_4);
     public static final Fortress fortress = new Fortress(MCVersion.v1_14_4);
     public static final Igloo igloo = new Igloo(MCVersion.v1_14_4);
     public static final ChunkRand rand = new ChunkRand();
+    public static final Random random = new Random();
 
     public static void main(String[] args) {
 
-        Long startSeed = 0L;
         int numFound = 0;
         final int toFind = 1000;
         Queue<FoundSeed> foundSeeds = new LinkedList<>();
 
-        seedloop: for (Long worldSeed = startSeed; worldSeed < 1L << 48; worldSeed++) {
+        seedloop:
+        while (true) {
+            long worldSeed = random.nextLong() + 1L;
 
-            if (!RandomChecker.isRandom(worldSeed)) {
-
-                // Fortress Check
-                @SuppressWarnings("unchecked")
-                RegionStructure.Data<Fortress> fortressData = (RegionStructure.Data<Fortress>) fortress.at(4, 4);
-                boolean fortressFound = fortress.canStart(fortressData, worldSeed, rand);
-                if (!fortressFound) {
-                    continue;
-                }
-
-                // Extra Setup
-                BiomeSource seedBiomeSource = new OverworldBiomeSource(MCVersion.v1_14_4, worldSeed);
-
-                // Village Check
-                int villageX = 0;
-                int villageZ = 0;
-
-                boolean villageFound = false;
-
-                outerloop: for (int x = 0; x < 8; x++) {
-                    for (int z = 0; z < 8; z++) {
-                        // x and z are chunk coordinates
-                        RegionStructure.Data<?> villageData = village.at(x, z);
-                        boolean possibleVillage = villageData.testStart(worldSeed, rand);
-                        if (possibleVillage) {
-                            Biome biomeAtPV = seedBiomeSource.getBiomeForNoiseGen(x * 4, 0, z * 4);
-
-                            if (biomeAtPV == Biome.SNOWY_TUNDRA) {
-                                villageFound = true;
-                                villageX = x;
-                                villageZ = z;
-                                break outerloop;
-                            }
-                        }
-                    }
-                }
-
-                if (!villageFound) {
-                    continue;
-                }
-
-                // Igloo Check
-                boolean foundIgloo = false;
-
-                outerloop: for (int x = -3; x < 4; x++) {
-                    for (int z = -3; z < 4; z++) {
-                        int ix = x + villageX;
-                        int iz = z + villageZ;
-
-                        RegionStructure.Data<?> iglooData = igloo.at(ix, iz);
-                        boolean possibleIgloo = iglooData.testStart(worldSeed, rand);
-
-                        if (possibleIgloo) {
-                            if (iglooData.testBiome(seedBiomeSource)) {
-                                foundIgloo = true;
-                                break outerloop;
-                            }
-                        }
-
-                    }
-                }
-
-                if (!foundIgloo) {
-                    continue;
-                }
-
-                FoundSeed thisFoundSeed = new FoundSeed(worldSeed, villageX * 16, villageZ * 16);
-                //System.out.println(thisFoundSeed);
-                foundSeeds.add(thisFoundSeed);
-                numFound++;
-
-                if (numFound % 50 == 0) {
-                    System.out.println(100.0 * ((float) numFound / toFind) + "%");
-                }
-
-                if (numFound == toFind) {
-                    File outputFile = new File("Tundra Village + Igloo " + Clock.systemUTC().millis() + ".txt");
-                    try {
-                        FileWriter outputWriter = new FileWriter(outputFile);
-                        for (FoundSeed foundSeed : foundSeeds) {
-                            outputWriter.write(foundSeed.toString() + "\n");
-                        }
-                        outputWriter.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-
-                    break seedloop;
-                }
-
+            // Fortress Check
+            @SuppressWarnings("unchecked")
+            RegionStructure.Data<Fortress> fortressData = (RegionStructure.Data<Fortress>) fortress.at(4, 4);
+            boolean fortressFound = fortress.canStart(fortressData, worldSeed, rand);
+            if (!fortressFound) {
+                continue;
             }
 
-        }
+            // Extra Setup
+            BiomeSource seedBiomeSource = new OverworldBiomeSource(MCVersion.v1_14_4, worldSeed);
 
+            // Village Check
+            int villageX = 0;
+            int villageZ = 0;
+
+            boolean villageFound = false;
+
+            outerloop:
+            for (int x = 0; x < 8; x++) {
+                for (int z = 0; z < 8; z++) {
+                    // x and z are chunk coordinates
+                    RegionStructure.Data<?> villageData = village.at(x, z);
+                    boolean possibleVillage = villageData.testStart(worldSeed, rand);
+                    if (possibleVillage) {
+                        Biome biomeAtPV = seedBiomeSource.getBiomeForNoiseGen(x * 4, 0, z * 4);
+
+                        if (biomeAtPV == Biome.SNOWY_TUNDRA) {
+                            villageFound = true;
+                            villageX = x;
+                            villageZ = z;
+                            break outerloop;
+                        }
+                    }
+                }
+            }
+
+            if (!villageFound) {
+                continue;
+            }
+
+            // Igloo Check
+            boolean foundIgloo = false;
+
+            outerloop:
+            for (int x = -3; x < 4; x++) {
+                for (int z = -3; z < 4; z++) {
+                    int ix = x + villageX;
+                    int iz = z + villageZ;
+
+                    RegionStructure.Data<?> iglooData = igloo.at(ix, iz);
+                    boolean possibleIgloo = iglooData.testStart(worldSeed, rand);
+
+                    if (possibleIgloo) {
+                        if (iglooData.testBiome(seedBiomeSource)) {
+                            foundIgloo = true;
+                            break outerloop;
+                        }
+                    }
+
+                }
+            }
+
+            if (!foundIgloo) {
+                continue;
+            }
+
+            FoundSeed thisFoundSeed = new FoundSeed(worldSeed, villageX * 16, villageZ * 16);
+            //System.out.println(thisFoundSeed);
+            foundSeeds.add(thisFoundSeed);
+            numFound++;
+
+            if (numFound % 50 == 0) {
+                System.out.println(100.0 * ((float) numFound / toFind) + "%");
+            }
+
+            if (numFound == toFind) {
+                File outputFile = new File("Tundra Village + Igloo " + Clock.systemUTC().millis() + ".txt");
+                try {
+                    FileWriter outputWriter = new FileWriter(outputFile);
+                    for (FoundSeed foundSeed : foundSeeds) {
+                        outputWriter.write(foundSeed.toString() + "\n");
+                    }
+                    outputWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                break seedloop;
+            }
+        }
     }
 }
--- src/main/java/ca/duncansserver/duoseedfinder/SavannaVillageTempleFinder.java
+++ src/main/java/ca/duncansserver/duoseedfinder/SavannaVillageTempleFinder.java
@@ -1,12 +1,5 @@
 package ca.duncansserver.duoseedfinder;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.time.Clock;
-
 import kaptainwutax.biomeutils.Biome;
 import kaptainwutax.biomeutils.source.BiomeSource;
 import kaptainwutax.biomeutils.source.OverworldBiomeSource;
@@ -17,115 +10,125 @@ import kaptainwutax.featureutils.structure.Village;
 import kaptainwutax.seedutils.mc.ChunkRand;
 import kaptainwutax.seedutils.mc.MCVersion;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Clock;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
+
 public class SavannaVillageTempleFinder {
 
     public static final Village village = new Village(MCVersion.v1_14_4);
     public static final Fortress fortress = new Fortress(MCVersion.v1_14_4);
     public static final DesertPyramid desertPyramid = new DesertPyramid(MCVersion.v1_14_4);
     public static final ChunkRand rand = new ChunkRand();
+    public static final Random random = new Random();
 
     public static void main(String[] args) {
 
-        Long startSeed = 0L;
         int numFound = 0;
         final int toFind = 1000;
         Queue<FoundSeed> foundSeeds = new LinkedList<>();
 
-        seedloop: for (Long worldSeed = startSeed; worldSeed < 1L << 48; worldSeed++) {
+        seedloop:
+        while (true) {
+            long worldSeed = random.nextLong() + 1L;
 
-            if (!RandomChecker.isRandom(worldSeed)) {
+            // Fortress Check
+            @SuppressWarnings("unchecked")
+            RegionStructure.Data<Fortress> fortressData = (RegionStructure.Data<Fortress>) fortress.at(4, 4);
+            boolean fortressFound = fortress.canStart(fortressData, worldSeed, rand);
+            if (!fortressFound) {
+                continue;
+            }
 
-                // Fortress Check
-                @SuppressWarnings("unchecked")
-                RegionStructure.Data<Fortress> fortressData = (RegionStructure.Data<Fortress>) fortress.at(4, 4);
-                boolean fortressFound = fortress.canStart(fortressData, worldSeed, rand);
-                if (!fortressFound) {
-                    continue;
-                }
+            // Extra Setup
+            BiomeSource seedBiomeSource = new OverworldBiomeSource(MCVersion.v1_14_4, worldSeed);
 
-                // Extra Setup
-                BiomeSource seedBiomeSource = new OverworldBiomeSource(MCVersion.v1_14_4, worldSeed);
+            // Village Check
+            int villageX = 0;
+            int villageZ = 0;
 
-                // Village Check
-                int villageX = 0;
-                int villageZ = 0;
+            boolean villageFound = false;
 
-                boolean villageFound = false;
+            outerloop:
+            for (int x = 0; x < 8; x++) {
+                for (int z = 0; z < 8; z++) {
+                    // x and z are chunk coordinates
+                    RegionStructure.Data<?> villageData = village.at(x, z);
+                    boolean possibleVillage = villageData.testStart(worldSeed, rand);
+                    if (possibleVillage) {
+                        Biome biomeAtPV = seedBiomeSource.getBiomeForNoiseGen(x * 4, 0, z * 4);
 
-                outerloop: for (int x = 0; x < 8; x++) {
-                    for (int z = 0; z < 8; z++) {
-                        // x and z are chunk coordinates
-                        RegionStructure.Data<?> villageData = village.at(x, z);
-                        boolean possibleVillage = villageData.testStart(worldSeed, rand);
-                        if (possibleVillage) {
-                            Biome biomeAtPV = seedBiomeSource.getBiomeForNoiseGen(x * 4, 0, z * 4);
-
-                            if (biomeAtPV == Biome.SAVANNA) {
-                                villageFound = true;
-                                villageX = x;
-                                villageZ = z;
-                                break outerloop;
-                            }
+                        if (biomeAtPV == Biome.SAVANNA) {
+                            villageFound = true;
+                            villageX = x;
+                            villageZ = z;
+                            break outerloop;
                         }
                     }
                 }
+            }
 
-                if (!villageFound) {
-                    continue;
-                }
+            if (!villageFound) {
+                continue;
+            }
 
-                // Temple Check
-                boolean foundDP = false;
+            // Temple Check
+            boolean foundDP = false;
 
-                outerloop: for (int x = -3; x < 4; x++) {
-                    for (int z = -3; z < 4; z++) {
-                        int px = x + villageX;
-                        int pz = z + villageZ;
+            outerloop:
+            for (int x = -3; x < 4; x++) {
+                for (int z = -3; z < 4; z++) {
+                    int px = x + villageX;
+                    int pz = z + villageZ;
 
-                        RegionStructure.Data<?> desertPyramidData = desertPyramid.at(px, pz);
-                        boolean possibleDP = desertPyramidData.testStart(worldSeed, rand);
+                    RegionStructure.Data<?> desertPyramidData = desertPyramid.at(px, pz);
+                    boolean possibleDP = desertPyramidData.testStart(worldSeed, rand);
 
-                        if (possibleDP) {
-                            if (desertPyramidData.testBiome(seedBiomeSource)) {
-                                foundDP = true;
-                                break outerloop;
-                            }
+                    if (possibleDP) {
+                        if (desertPyramidData.testBiome(seedBiomeSource)) {
+                            foundDP = true;
+                            break outerloop;
                         }
-
-                    }
-                }
-
-                if (!foundDP) {
-                    continue;
-                }
-
-                FoundSeed thisFoundSeed = new FoundSeed(worldSeed, villageX * 16, villageZ * 16);
-                //System.out.println(thisFoundSeed);
-                foundSeeds.add(thisFoundSeed);
-                numFound++;
-
-                if (numFound % 50 == 0) {
-                    System.out.println(100.0 * ((float) numFound / toFind) + "%");
-                }
-
-                if (numFound == toFind) {
-                    File outputFile = new File("Savanna Village + Temple " + Clock.systemUTC().millis() + ".txt");
-                    try {
-                        FileWriter outputWriter = new FileWriter(outputFile);
-                        for (FoundSeed foundSeed : foundSeeds) {
-                            outputWriter.write(foundSeed.toString() + "\n");
-                        }
-                        outputWriter.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
                     }
 
-                    break seedloop;
+                }
+            }
+
+            if (!foundDP) {
+                continue;
+            }
+
+            FoundSeed thisFoundSeed = new FoundSeed(worldSeed, villageX * 16, villageZ * 16);
+            //System.out.println(thisFoundSeed);
+            foundSeeds.add(thisFoundSeed);
+            numFound++;
+
+            if (numFound % 50 == 0) {
+                System.out.println(100.0 * ((float) numFound / toFind) + "%");
+            }
+
+            if (numFound == toFind) {
+                File outputFile = new File("Savanna Village + Temple " + Clock.systemUTC().millis() + ".txt");
+                try {
+                    FileWriter outputWriter = new FileWriter(outputFile);
+                    for (FoundSeed foundSeed : foundSeeds) {
+                        outputWriter.write(foundSeed.toString() + "\n");
+                    }
+                    outputWriter.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
 
+                break seedloop;
             }
 
         }
 
     }
+
 }
+
